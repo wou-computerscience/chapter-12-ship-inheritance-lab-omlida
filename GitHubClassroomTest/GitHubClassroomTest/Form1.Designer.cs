45723c8c679274b1b2592a34c14758a23cc6353c
Allison Omlid <omlida@lobo.wou.edu>
2026-05-06 12:14:10 -0700

    Added Exit button

--- GitHubClassroomTest/GitHubClassroomTest/Form1.Designer.cs
+++ GitHubClassroomTest/GitHubClassroomTest/Form1.Designer.cs
@@ -28,18 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(188, 143);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "Exit";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "GitHub Classroom";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
--- GitHubClassroomTest/GitHubClassroomTest/Form1.cs
+++ GitHubClassroomTest/GitHubClassroomTest/Form1.cs
@@ -6,5 +6,10 @@ namespace GitHubClassroomTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
